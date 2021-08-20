91e248131c6c92e5fc755d9f89b4a8e095f2d800
Maria Eduarda <62070595+marialeao@users.noreply.github.com>
2021-08-19 21:19:36 -0300

    Add files via upload

--- tarefas.py
+++ tarefas.py
@@ -1,26 +1,51 @@
-def add_tarefa(codigo, tarefa):
-    tarefas.update({codigo, tarefa})
-    
-tarefas = {1: 'prova de matemática', 2: 'tarefa de português'}
-resposta = 0
+from pessoa import Pessoa
 
-while resposta != '4':
-    print('-='*15)
-    print('[1] Adicionar nova tarefa')
-    print('[2] Mostrar tarefas')
-    print('[3] Excluir tarefa')
-    print('[4] Sair')
+class Tarefa(object):
     
-    resposta = input('O que deseja fazer? ')
+    def __init__(self, id: int, descricao: str, realizada: bool, data_execucao: str, pessoa: Pessoa) -> None:
+        self.__id = id
+        self.__descricao = descricao
+        self.__realizada = realizada
+        self.__data_execucao = data_execucao
+        self.__pessoa = pessoa
 
-    if resposta == '1':
-        tarefa = input('Nome da tarefa: ')
-        codigo = input('Código da tarefa: ')
-        add_tarefa(codigo, tarefa)
-    elif resposta == '2':
-        print(tarefas)
-    elif resposta == '3':
-        delete = input ('Digite o código da tarefa que deseja deletar: ')
-        del(tarefas[int(delete)])
-    elif resposta == '4':
-        print('Programa finalizado com suceso :)')
+    def get_id(self) -> int:
+        return self.__id
+
+    def set_descricao(self, descricao: str) -> None:
+        self.__descricao = descricao
+
+    def get_descricao(self) -> str:
+        return self.__descricao
+
+    def set_realizada(self, realizada: bool) -> None:
+        self.__realizada = realizada
+    
+    def get_realizada(self) -> bool:
+        return self.__realizada
+
+    def set_data_execucao(self, data_execucao: str) -> None:
+        self.__data_execucao = data_execucao
+
+    def get_data_execucao(self) -> str:
+        return self.__data_execucao
+
+    def __repr__(self) -> str:
+        return "Tarefa -> {0} - {1} - {2} - {3} {4}".format(self.__id, self.__descricao, self.__data_execucao, "Sim" if self.__realizada else "Não", self.__pessoa.get_nome())
+
+    def json(self): 
+        return {
+        "id": self.__id, 
+        "descricao": self.__descricao, 
+        "realizada": self.__realizada, 
+        "data_execucao": self.__data_execucao,
+        "pessoa.id": self.__pessoa.get_id(),
+        "pessoa.nome": self.__pessoa.get_nome(),
+        "pessoa.email": self.__pessoa.get_email()
+        }
+
+pessoa = Pessoa(1, "Maria Eduarda", "dudaleiteleao@gmail.com")
+tarefa = Tarefa(10, "prova de programação", True, "22/10/2021", pessoa)
+print(repr(tarefa))
+print(pessoa.json())
+print(tarefa.json())
